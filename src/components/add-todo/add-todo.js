a88da731194b375dbddce350bb369d9ac7aca9c5
aibrohim <ibrohim7840@gmail.com>
2021-10-25 12:41:43 +0500

    fadsf

---
+++ src/components/add-todo/add-todo.js
@@ -0,0 +1,31 @@
+import { useRef } from "react";
+import "./add-todo.scss";
+
+const AddTodo = ({todos, setTodos}) => {
+  const inputRef = useRef();
+  const handleFormSubmit = (evt) => {
+    evt.preventDefault();
+
+    if (inputRef.current.value.trim()) {
+      setTodos([
+        ...todos,
+        {
+          id: Math.random(),
+          name: inputRef.current.value,
+          completed: false
+        }
+      ]);
+      inputRef.current.value = ""
+    }
+    // Name, Description, Tags (<- 1ta input) (umumiy 3ta input)
+  };
+
+  return (
+    <form onSubmit={handleFormSubmit} className="add-todo" action="#">
+      <input ref={inputRef} placeholder="Todo name" className="add-todo__input" type="text" />
+      <button className="add-todo__submit" type="submit" >Add todo</button>
+    </form>
+  );
+};
+
+export default AddTodo;
